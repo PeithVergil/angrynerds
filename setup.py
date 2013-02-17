6692dde906aa7ae389dbef4971a57ffab7f540e8
Peith Vergil <peith.vergil@gmail.com>
2013-02-17 12:55:00 +0800

    Added author name and version number in the setup.py script.

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from distutils.core import setup
 import py2exe
 
 includes = []
-excludes = []
+excludes = ['email']
 packages = []
 
+dll_excludes = []
+
 setup(
+	name = 'AngryNerds',
+	author = 'Peith Vergil',
+	version = '0.0.1',
 	options = {
 		'py2exe': {
 			'optimize'     : 2,
@@ -15,6 +20,7 @@ setup(
 			'packages'     : packages,
 			'compressed'   : 2,
 			'bundle_files' : 1,
+			'dll_excludes' : dll_excludes,
 		}
 	},
 	windows = [
